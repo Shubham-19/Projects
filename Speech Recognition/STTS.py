1fbac60a2becbdcc98a60350485285ebed308171
Shubham Sharma <sassyshubham@gmail.com>
2019-05-28 12:36:07 +0530

    Create STTS.py

---
+++ Speech Recognition/STTS.py
@@ -0,0 +1,36 @@
+from translate import Translator
+import Wit as wit
+import win32com.client
+
+def main():
+    speaker = win32com.client.Dispatch('SAPI.SpVoice')
+    if __name__ == '__main__':
+        try:
+            print("Welcome to the Python Translator")
+            speaker.Speak("Welcome to the Python Translator")
+            
+            print("What language would you like your text translated to?")
+            speaker.Speak("What language would you like your text translated to?")
+            lang = wit.talk().lower()
+            speaker.Speak('You chose {}'.format(lang))
+            print("Say something to the bot to convert...")
+            speaker.Speak("Say something to the bot to convert...")
+            
+            print("Human: ")
+            request = wit.talk().lower()
+            if(request.lower() == 'exit'):
+                print('Getting you otta here!')
+                speaker.Speak('You have requested to exit, so we are getting you otta here! We hope you got your query resolved, Thank you for using our service')
+
+                return 0
+            else:
+                Tl = Translator(to_lang = lang)
+                line = Tl.translate(request)
+                print("Bot: " + line)
+                speaker.Speak('{} in {} sounds like {}'.format(request, lang, line))
+            
+            main()
+        except KeyboardInterrupt:        
+            print("Program Interrupted")
+
+main()
